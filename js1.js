e8560d1a06aea6db75fc79185ed38e6979d30e6f
sumit singh tomar <sumitsinght10@gmail.com>
2024-01-29 11:05:26 +0530

    delete user

--- js1.js
+++ js1.js
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
         editButton.className = "editBtn"
         button.textContent = "delete"
         editButton.textContent = "Edit"
-        button.id = res.data.email
-        editButton.name = res.data.email
+        button.id = item._id
+        editButton.name = item.email
         li.appendChild(button)
         li.appendChild(editButton)
         ul.appendChild(li)
@@ -34,38 +34,35 @@ form.addEventListener("submit", function (event) {
     "phone": event.target.phone.value
   }
 
-  // const username=document.getElementById('username')
-
-
   axios.post("https://crudcrud.com/api/ce29ece0a2314de991e22b1cce600613/booking", userDetail)
     .then((res) => console.log('success', res))
     .catch((e) => console.log('error', e))
 
-  let detail = JSON.stringify(userDetail)
-  let email = event.target.email.value
-  localStorage.setItem(email, detail)
+  // let detail = JSON.stringify(userDetail)
+  // let email = event.target.email.value
+  // localStorage.setItem(email, detail)
 
-  const ul = document.querySelector('ul')
-  ul.className = "showDetail"
+  // const ul = document.querySelector('ul')
+  // ul.className = "showDetail"
 
-  const li = document.createElement('li')
-  li.textContent = `${event.target.username.value} - ${event.target.email.value} ${event.target.phone.value}`
+  // const li = document.createElement('li')
+  // li.textContent = `${event.target.username.value} - ${event.target.email.value} ${event.target.phone.value}`
 
-  const button = document.createElement('button')
-  const editButton = document.createElement('button')
+  // const button = document.createElement('button')
+  // const editButton = document.createElement('button')
 
-  button.className = "deleteBtn"
-  editButton.className = "editBtn"
+  // button.className = "deleteBtn"
+  // editButton.className = "editBtn"
 
-  button.textContent = "delete"
-  editButton.textContent = "Edit"
+  // button.textContent = "delete"
+  // editButton.textContent = "Edit"
 
-  button.id = event.target.email.value
-  editButton.name = event.target.email.value
+  // button.id = event.target.email.value
+  // editButton.name = event.target.email.value
 
-  li.appendChild(button)
-  li.appendChild(editButton)
-  ul.appendChild(li)
+  // li.appendChild(button)
+  // li.appendChild(editButton)
+  // ul.appendChild(li)
 
   event.target.username.value = ''
   event.target.email.value = ''
@@ -79,7 +76,15 @@ showDetail.addEventListener('click', function (event) {
   if (event.target.classList.contains("deleteBtn")) {
     let elementToRemove = event.target.parentElement;
     showDetail.removeChild(elementToRemove);
-    localStorage.removeItem(event.target.id);
+    
+    axios.delete(`https://crudcrud.com/api/ce29ece0a2314de991e22b1cce600613/booking/${event.target.id}`)
+      .then((res)=>{
+        alert('user delete successfully')
+      }).catch((e)=>{
+        alert('failed')
+        console.log('eeeeeeeeee',e)
+      })
+
   }
 
   if (event.target.classList.contains("editBtn")) {
